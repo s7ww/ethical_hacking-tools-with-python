ba841357084d4a31c2327c4fb75a4315628f4a11
Sawan Warkar <165464766+s7ww@users.noreply.github.com>
2024-10-09 19:56:23 +0530

    Update mac_changer.py

--- mac_changer_using_cmdline_args/mac_changer.py
+++ mac_changer_using_cmdline_args/mac_changer.py
@@ -1,25 +1,28 @@
-#!/usr/bin/env python3
-
 import subprocess
-import optparse  # to create command line arguement
-
-parser = optparse.OptionParser() #object to hanlde user cmd line input
-
-parser.add_option("-i","--interface",dest="interface",help="interface to change mac address")
-parser.add_option("-m","--mac",dest="new_mac",help=" new mac address")
-#using the method provided by the object
-
-(options, arguments)= parser.parse_args() #seperates the user input and assigns to respective variales
+import optparse
 
 
-# Get interface and new MAC address from user
-interfacez = options.interface
-new_mac = options.new_mac
+def get_args():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    (options, arguments) = parser.parse_args()
 
-# Change the MAC address
-subprocess.call(f"sudo ifconfig {interfacez} down", shell=True)
-subprocess.call(f"sudo ifconfig {interfacez} hw ether {new_mac}", shell=True)
-subprocess.call(f"sudo ifconfig {interfacez} up", shell=True)
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new MAC address, use --help for more info.")
 
-# Display the current network configuration
-subprocess.call("ip addr show", shell=True)
+    return options
+
+
+def change_mac(interface, new_mac):
+    print(f"[+] Changing MAC address for {interface} to {new_mac}")
+    subprocess.call(["sudo", "ifconfig", interface, "down"])
+    subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["sudo", "ifconfig", interface, "up"])
+
+
+options = get_args()
+change_mac(options.interface, options.new_mac)
+
